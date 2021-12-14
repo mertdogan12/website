513ce778b9e891ddfa0857262d4bd8bfe98ce85e
Mert Dogan <mert.dogan@me.com>
2021-12-14 22:23:43 +0100

    command line update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,33 @@
 import { KeyboardEvent, useState } from "react";
 import CommandLine from "./CommandLine";
 
+type Command = {
+  command: string;
+  output: string;
+};
+
 const Home = () => {
-  const [log, setLog] = useState<string[]>([]);
+  const [log, setLog] = useState<Command[]>([]);
+  let userName: string = "guest";
+  let computerName: string = "NaN";
 
   const input = (e: KeyboardEvent<HTMLInputElement>): void => {
     if (e.key === "Enter") {
       const target: HTMLInputElement = e.target as HTMLInputElement;
       if (target.value) {
-        setLog([...log, target.value]);
+        let command: Command = { command: target.value, output: "" };
+
+        switch (target.value) {
+          case "clear":
+            setLog([]);
+            break;
+
+          default:
+            command.output = "Command not found: " + target.value;
+
+            setLog([...log, command]);
+        }
+
         target.value = "";
       }
     }
@@ -16,12 +35,21 @@ const Home = () => {
 
   return (
     <div>
-      <div>
+      <div className="home" id="log">
         {log.map((value, index) => {
-          return <p key={index}>{value}</p>;
+          return (
+            <div>
+              <p className="home" id="cmdLine">
+                {userName}@{computerName} {">"} {value.command}
+              </p>
+              <p className="home" key={index}>
+                {value.output}
+              </p>
+            </div>
+          );
         })}
       </div>
-      <CommandLine onKeyDown={input} name="guest" computer="NaN" />
+      <CommandLine onKeyDown={input} name={userName} computer={computerName} />
     </div>
   );
 };
