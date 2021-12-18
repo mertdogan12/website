fb40a61424ca5637fa31c401c59666658ebb2850
Mert Dogan <mert.dogan@me.com>
2021-12-18 18:53:24 +0100

    master --> stable

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Info from "./pages/Info";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/info" element={<Info />} />
       </Routes>
     </BrowserRouter>
   );
