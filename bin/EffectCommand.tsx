224a56eb4922eea392fd06d003e678245979d4e0
Mert Dogan <mert.dogan@me.com>
2022-03-31 19:09:10 +0200

    Error fix

--- bin/EffectCommand.tsx
+++ bin/EffectCommand.tsx
@@ -4,7 +4,12 @@ const EchoCommand: CommandInterface = {
   command: "effect",
   description: "Change the terminal effect",
   exe: (input: string[]): string => {
-    document.getElementById("effectMain")?.className = input[0] + "Effect";
+    const effectMain: HTMLElement | null =
+      document.getElementById("effectMain");
+    if (effectMain == null)
+      return "Error: div with id 'effectMain' does not exist";
+
+    effectMain.className = input[0].toLowerCase() + "Effect";
 
     return "Effect: " + input[0];
   },
