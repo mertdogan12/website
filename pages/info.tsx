f4e75d41422a912ac689024b641d3b5b798e60e1
Mert Dogan <mert.dogan@me.com>
2022-04-03 23:45:42 +0200

    info update

--- pages/info.tsx
+++ pages/info.tsx
@@ -26,16 +26,25 @@ const Info = () => {
       .then((data) => setJson(data));
   }, []);
 
+  const Icon = (props: { iconUrl: string }) => {
+    if (props.iconUrl === "")
+      return <div className={styles.socialElementIcon} />;
+    else
+      return <img src={props.iconUrl} className={styles.socialElementIcon} />;
+  };
+
   return (
     <div className={styles.socials}>
       {json.socials.map((value, index) => {
         return (
-          <div className={styles.socialElement} key={index}>
-            <div className={styles.socialElementIcon}>
-              {() => {
-                if (value.icon !== "") return <img src={value.icon} />;
-              }}
-            </div>
+          <div
+            className={styles.socialElement}
+            key={index}
+            onClick={() => {
+              window.open(value.link, "_blank");
+            }}
+          >
+            <Icon iconUrl={value.icon} />
             <a className={styles.socialElementLink} href={value.link}>
               {value.name}
             </a>
